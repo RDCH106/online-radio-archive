0b8257a0f2b32cf71b3a1da494231a5328a63d13
rdecelis <rdecelis@stt-systems.com>
2018-09-18 09:37:34 +0200

    Add get_files

--- ALL_generator.py
+++ ALL_generator.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from os import listdir
-from os.path import isfile, join
+from os import listdir, path
 
 logo ="""
                        !
@@ -28,12 +27,15 @@ logo ="""
         
 print(logo)
 
-generes = ["Electronic", "JPop", "Soundtrack"]
+genres = ["Electronic", "JPop", "Soundtrack"]
 repo_base_url = "https://raw.githubusercontent.com/RDCH106/online-radio-archive/master"
 
-def get_files(genere):
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+def get_files(genre):
+    pwd = path.dirname(path.abspath(__file__))
+    onlyfiles = [f for f in listdir(path.join(pwd, genre)) if path.isfile(path.join(pwd, genre, f))]
+    return onlyfiles
 
 
-for genere in generes:
-    pass
+for genre in genres:
+    print(get_files(genre))
